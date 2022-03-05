866e438f5fbe3cda85f9097f862ebe1cfe161438
henriquemgn <32944009+henriquemgn@users.noreply.github.com>
2022-03-05 03:57:57 -0300

    Update BuyPotions.lua

--- BuyPotions.lua
+++ BuyPotions.lua
@@ -1,24 +1,77 @@
+
 --buy 1000 mana potions
 
 local npc = getCreatureByName("Rachel")
 
 if not npc then 
+
   return false 
+
 end
 
 local pos = player:getPosition()
+
 local npcPos = npc:getPosition()
 
 if math.max(math.abs(pos.x - npcPos.x), math.abs(pos.y - npcPos.y)) > 3 then
+
   autoWalk(npcPos, {precision=3})
+
   delay(300)
+
   return "retry"
+
 end
 
 if not NPC.isTrading() then
-  NPC.say("hi")
+
+  NPC.say("hi trade")
+
   delay(1000)
-  NPC.say("trade")
+
+  NPC.say("hi trade")
+
   delay(1000)
+
   return "retry"
+
 end
+
+delay(200)
+
+--check supply
+
+local itemID = 238
+
+local minmpcount = 1000
+
+local totalMp = 0
+
+for _, container in pairs(getContainers()) do
+
+  for _, item in ipairs(container:getItems()) do
+
+    
+
+    if (item:getId() == itemID) then
+
+      
+
+      totalMp = totalMp + item:getCount() 
+
+    end
+
+  end
+
+end
+
+info(totalMp)
+
+if totalMp <= minmpcount then
+
+  NPC.buy(238, 100)
+
+  return "retry"
+
+end
+
